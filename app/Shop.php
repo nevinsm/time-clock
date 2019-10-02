6a76b898bfd1d38cc3cf26421c093eb55dd3873e
Nevin Morgan <nevins.morgan@gmail.com>
2019-10-01 21:20:55 -0400

    created data model, and added spatie/opening-hours for easy open hours manipulation

---
+++ app/Shop.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Shop extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'location', 'open_hours'];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'open_hours' => 'array',
+    ];
+}
